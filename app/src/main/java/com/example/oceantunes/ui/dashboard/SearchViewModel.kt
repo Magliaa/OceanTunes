521333f53a2bd24214ce350fb5ee59b27e9475fa
Tsurao <picknick1612@gmail.com>
2024-10-24 18:24:50 +0200

    :sparkles: navbar modificata, icona app, refactor dei nomi sbagliati

--- app/src/main/java/com/example/oceantunes/ui/dashboard/SearchViewModel.kt
+++ app/src/main/java/com/example/oceantunes/ui/dashboard/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.oceantunes.ui.notifications
+package com.example.oceantunes.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/oceantunes/ui/notifications/ProfileFragment.kt
+++ app/src/main/java/com/example/oceantunes/ui/notifications/ProfileFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.oceantunes.databinding.FragmentNotificationsBinding
+import com.example.oceantunes.databinding.FragmentProfileBinding
 
-class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val profileViewModel =
+            ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
