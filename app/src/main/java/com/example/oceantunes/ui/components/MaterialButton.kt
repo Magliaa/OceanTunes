606c6015cb8bca8e874f6c866db8b391915cf28a
Luca Perego <116523264+dotLuxPI@users.noreply.github.com>
2024-10-24 15:19:45 +0200

    :sparkles: Material Button

---
+++ app/src/main/java/com/example/oceantunes/ui/components/MaterialButton.kt
@@ -0,0 +1,48 @@
+package com.example.oceantunes.ui.components
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import androidx.core.content.ContextCompat
+import com.example.oceantunes.R
+import com.google.android.material.button.MaterialButton
+
+
+class MaterialButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = com.google.android.material.R.attr.materialButtonStyle
+) : MaterialButton(context, attrs, defStyleAttr) {
+
+    init {
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.MaterialButton)
+        val buttonText = attributes.getString(R.styleable.MaterialButton_buttonText)
+        val buttonColor = attributes.getColorStateList(R.styleable.MaterialButton_buttonColor)
+        val setIcon = attributes.getBoolean(R.styleable.MaterialButton_setIcon, false)
+
+        text = buttonText ?: "DEFAULT TEXT BUTTON"
+        textSize = resources.getDimension(R.dimen.button_text_size)
+        cornerRadius = resources.getDimensionPixelSize(R.dimen.button_corner_radius)
+        backgroundTintList = buttonColor ?:
+                ColorStateList.valueOf(ContextCompat.getColor(context, R.color.purple3))
+
+        if (buttonColor == null) {
+            setTextColor(ContextCompat.getColor(context, R.color.dark))
+        } else {
+            setTextColor(ContextCompat.getColor(context, R.color.white))
+        }
+
+        if (setIcon) {
+            icon = ContextCompat.getDrawable(context, R.drawable.ic_arrow_right)
+            iconSize = resources.getDimensionPixelSize(R.dimen.button_icon_size)
+
+            iconTint = if (buttonColor == null) {
+                ColorStateList.valueOf(ContextCompat.getColor(context, R.color.purple2))
+            } else {
+                ColorStateList.valueOf(ContextCompat.getColor(context, R.color.white))
+            }
+        }
+
+        attributes.recycle()
+    }
+}
