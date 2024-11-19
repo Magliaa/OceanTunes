ebb92265f334d293dd0c9aa1bdcec45464274ad5
Luca Perego <116523264+dotLuxPI@users.noreply.github.com>
2024-11-19 15:43:22 +0100

    :sparkles: Divider

--- app/src/main/java/com/tunagold/oceantunes/ui/components/MaterialFAB.kt
+++ app/src/main/java/com/tunagold/oceantunes/ui/components/MaterialFAB.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.util.AttributeSet
-import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.tunagold.oceantunes.R
 
