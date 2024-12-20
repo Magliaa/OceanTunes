3e81f79eb4a0e1a90523309c290fdf0a541e83bd
Luca Perego <116523264+dotLuxPI@users.noreply.github.com>
2024-12-20 14:38:26 +0100

    🐛 Drawer toggle fix
    Settings toggle is now working.
    Settings position is now dependant from elipses position.
    Small indentation fix + similar...

--- app/src/main/java/com/tunagold/oceantunes/ui/components/Elipses.kt
+++ app/src/main/java/com/tunagold/oceantunes/ui/components/Elipses.kt
@@ -11,31 +11,32 @@ class Elipses @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : androidx.appcompat.widget.AppCompatImageButton (context, attrs, defStyleAttr) {
 
-
+    private var settings: View? = null
 
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.Elipses)
-
-        val elipsesIcon = attributes.getResourceId(R.styleable.Elipses_elipsesIcon, R.drawable.fab_logo_temp)
+        val elipsesIcon = attributes.getResourceId(R.styleable.Elipses_elipsesIcon, R.drawable.ic_vertdots_fillblack_24dp)
 
         setImageResource(elipsesIcon)
 
-        val settings = findViewById<Settings>(R.id.settingsCard)
-
-        settings.visibility = View.GONE
-
-        setOnClickListener {
-            settings.visibility =
-                if(settings.visibility == View.VISIBLE
-            ) View.GONE else View.VISIBLE
-        }
-
-
-
         attributes.recycle()
+
+        // Set click event to toggle the visibility of the settings card
+        setOnClickListener {
+            settings?.let {
+                it.visibility =
+                    if (it.visibility == View.VISIBLE) View.GONE
+                    else View.VISIBLE
+            }
+        }
     }
 
+    // Attach the listener when the view is attached to the window
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
 
-
-
+        val parentView = parent as? View
+        settings = parentView?.findViewById(R.id.settingsCard)
+        settings?.visibility = View.GONE
+    }
 }
