249bfedd6714880132cee99285a0e87e6d24da84
Tsurao <picknick1612@gmail.com>
2025-06-11 15:09:04 +0200

    💄 removed dark mode

--- app/src/main/java/com/tunagold/oceantunes/MainActivity.kt
+++ app/src/main/java/com/tunagold/oceantunes/MainActivity.kt
@@ -17,8 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/tunagold/oceantunes/OceanTunesClass.kt
+++ app/src/main/java/com/tunagold/oceantunes/OceanTunesClass.kt
@@ -1,7 +1,13 @@
 package com.tunagold.oceantunes
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class OceanTunesApp : Application()
+class OceanTunesApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
